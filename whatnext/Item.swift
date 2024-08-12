8bb0f95cd5596ae37afdf025f96ac749d01a6861
Siddharth Ajay <sid.ajay05@gmail.com>
2024-08-11 17:12:14 -0700

    Initial Commit

---
+++ whatnext/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  whatnext
+//
+//  Created by Sid Ajay on 8/11/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ whatnext/whatnextApp.swift
@@ -0,0 +1,32 @@
+//
+//  whatnextApp.swift
+//  whatnext
+//
+//  Created by Sid Ajay on 8/11/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct whatnextApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
