b0864a9dae7e5197d74c71ff37388cac876ee857
Siddharth Ajay <sid.ajay05@gmail.com>
2024-08-13 01:22:06 -0700

    light mode and lefty mode

--- whatnext/ContentView.swift
+++ whatnext/ContentView.swift
@@ -7,32 +7,74 @@
 
 import SwiftUI
 import SwiftData
+let dark_colors = [
+    "bg" : Color(.black),
+    "inactive" : Color(.gray),
+    "clock" : Color(.green),
+    "contrast" : Color(.white),
+    "prompt" : Color(.gray)
+]
+let light_colors = [
+    "bg" : Color(.white),
+    "inactive" : Color(.gray),
+    "clock" : Color(.green),
+    "contrast" : Color(.black),
+    "prompt" : Color(.gray)
+]
 
-let bg_color = Color(.black)
-let inactive = Color(.gray)
-let clock_color = Color(.green)
-let contrast_color = Color(.white)
-let prompt_color = Color(.gray)
+class UserSettings: ObservableObject {
+    let defaults = UserDefaults.standard
+    
+    @Published var light_mode: Bool
+    @Published var color_scheme = dark_colors
+    @Published var lefty = false
+    
+    init() {
+        // set defaults if not previuously set
+        let initialized = defaults.bool(forKey: "initialized")
+        if (!initialized) {
+            defaults.set(false, forKey: "light_mode")
+            defaults.set(false, forKey: "lefty")
+            defaults.set(true, forKey: "initialized")
+        }
+        // check cached settings
+        light_mode = defaults.bool(forKey: "light_mode")
+        color_scheme = light_mode ? light_colors : dark_colors
+        lefty = defaults.bool(forKey: "lefty")
+    }
+    
+    func toggleLightMode() {
+        light_mode = !light_mode
+        defaults.set(light_mode, forKey: "light_mode")
+        color_scheme = light_mode ? light_colors : dark_colors
+    }
+    
+    func toggleLefty() {
+        lefty = !lefty
+        defaults.set(lefty, forKey: "lefty")
+    }
+    
+}
 
 struct TaskRow: View {
+    @EnvironmentObject var settings: UserSettings
     @Bindable var task: Task
     var body: some View {
         HStack {
 
             Text("")
             TextField("", text: $task.content,
-                      prompt: Text("What's next?")
-                        .foregroundStyle(prompt_color)
-                        /*.opacity(0.5)*/)
-                .foregroundStyle(contrast_color)
+                prompt: Text("What's next?")
+                    .foregroundStyle(settings.color_scheme["prompt"]!))
+                .foregroundStyle(settings.color_scheme["contrast"]!)
                 .multilineTextAlignment(.center)
-                .tint(contrast_color)
+                .tint(settings.color_scheme["contrast"])
             if task.timed {
                 HStack {
                     Spacer()
                         .padding(.horizontal)
                     ZStack {
-                        Text(task.time,style:.time).foregroundStyle(contrast_color).opacity(0.5)
+                        Text(task.time,style:.time).foregroundStyle(settings.color_scheme["contrast"]!).opacity(0.5)
                         DatePicker("", selection: $task.time, displayedComponents: .hourAndMinute).opacity(0.01000001)
                     }
                 }
@@ -43,12 +85,12 @@ struct TaskRow: View {
             } label: {
                 if task.alarm {
                     Image(systemName: "alarm").resizable()
-                        .foregroundColor(clock_color)
+                        .foregroundColor(settings.color_scheme["clock"])
                         .bold()
                 }
                 else {
                     Image(systemName: "clock").resizable()
-                        .foregroundColor(task.timed ? clock_color : inactive)
+                        .foregroundColor(task.timed ? settings.color_scheme["clock"] : settings.color_scheme["inactive"])
                         .bold()
                 }
             }.buttonStyle(PlainButtonStyle())
@@ -59,22 +101,22 @@ struct TaskRow: View {
 }
 
 struct ContentView: View {
+    @EnvironmentObject var settings: UserSettings
     @State var s_val = 0.0
-    @State var blur = 0.0
     var body: some View {
         ZStack {
-            bg_color
+            settings.color_scheme["bg"]
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            PrimaryView(s_val: $s_val).blur(radius: s_val*2)//.animation(.snappy, value: blur)
+            PrimaryView(s_val: $s_val).blur(radius: s_val*3)
             if s_val == 1 {
                 SettingsView(s_val: $s_val)
-                    //.transition(.opacity)// (scale: 0, anchor: .bottomTrailing))
             }
         }
     }
 }
 
 struct PrimaryView: View {
+    @EnvironmentObject var settings: UserSettings
     @Binding var s_val: Double
     @Environment(\.modelContext) var modelContext
     @Query var tasks: [Task]
@@ -86,7 +128,7 @@ struct PrimaryView: View {
             Text("What Next")
                 .bold(true)
                 .font(.title)
-                .foregroundStyle(contrast_color)
+                .foregroundStyle(settings.color_scheme["contrast"]!)
             
             Spacer()
             
@@ -106,8 +148,8 @@ struct PrimaryView: View {
                                 if task.timed {task.toggleAlarm()}
                             }.tint(task.timed ? .blue : .gray)
                         }
-                }.listRowBackground(bg_color)
-                .listRowSeparatorTint(contrast_color)
+                }.listRowBackground(settings.color_scheme["bg"])
+                .listRowSeparatorTint(settings.color_scheme["prompt"])
             }.listStyle(.plain)
             .scrollContentBackground(.hidden)
             
@@ -116,7 +158,7 @@ struct PrimaryView: View {
             HStack(alignment: .bottom) {
                 
                 Text(String(tasks.count) + " tasks")
-                    .foregroundStyle(contrast_color)
+                    .foregroundStyle(settings.color_scheme["contrast"]!)
                     .padding(.leading, 20.0)
                     .opacity(0.5)
                     .frame(maxWidth:80)
@@ -128,7 +170,7 @@ struct PrimaryView: View {
                 } label: {
                     Image(systemName: "plus.circle")
                         .resizable()
-                        .foregroundStyle(contrast_color)
+                        .foregroundStyle(settings.color_scheme["contrast"]!)
                         .frame(width: 50, height: 50)
                         .buttonStyle(.plain)
                 }
@@ -137,24 +179,24 @@ struct PrimaryView: View {
                 Spacer()
                 
 
-                    Button{
-                        withAnimation(Animation.easeOut(duration: 0.5)) {
-                            s_val = 1
-                        }
-                        // summon settings: flip, zoom, help,esl color,
-                    } label: {
-                        if s_val == 0 {
-                            Image(systemName: "gearshape")
-                                .resizable()
-                                .frame(width: 20.0, height: 20.0)
-                                .foregroundStyle(contrast_color)
-                                .animated_flip(axis: (-1,-1,0))
-                        }
+                Button{
+                    withAnimation(Animation.easeOut(duration: 0.5)) {
+                        s_val = 1
                     }
-                    .padding(.trailing, 20.0)
+                } label: {
+                    if s_val == 0 {
+                        Image(systemName: "gearshape")
+                            .resizable()
+                            .frame(width: 20.0, height: 20.0)
+                            .foregroundStyle(settings.color_scheme["contrast"]!)
+                            .animated_flip(axis: (-1,-1,0))
+                    }
+                }
+                .padding(.trailing, 20.0)
                 .frame(maxWidth:80, maxHeight: 20)
                 
             }.frame(height:50)
+            .environment(\.layoutDirection, settings.lefty ? .rightToLeft : .leftToRight)
         }.padding()
     }
     
@@ -168,31 +210,53 @@ struct PrimaryView: View {
 }
 
 struct SettingsView: View {
+    @EnvironmentObject var settings: UserSettings
     @Binding var s_val: Double
     @State var anim: Double = 0
     
     var body: some View {
+        // TODO: settings -> help
         HStack() {
             Spacer()
             VStack(alignment: .center) {
                 Spacer()
-                Button{
+                Button {
+                    //help
+                } label: {
+                    Image(systemName: "questionmark.circle")
+                        .settings_icon(color: settings.color_scheme["contrast"]!)
+                }
+                Spacer()
+                    .frame(height:40)
+                Button {
+                    withAnimation(.bouncy) {
+                        settings.toggleLefty()
+                    }
+                } label: {
+                    Image(systemName: settings.lefty ? "circle.righthalf.filled" : "circle.lefthalf.filled")
+                        .settings_icon(color: settings.color_scheme["contrast"]!)
+                }
+                Spacer()
+                    .frame(height:40)
+                Button {
+                    settings.toggleLightMode()
+                } label: {
+                    Image(systemName: settings.light_mode ? "lightbulb.circle.fill" : "lightbulb.circle")
+                        .settings_icon(color: settings.color_scheme["contrast"]!)
+                }
+                Spacer()
+                    .frame(height:40)
+                Button {
                     withAnimation(Animation.easeOut(duration: 1)) {
-                        s_val = 0
+                        s_val = 0 // TODO: exit animation
                     }
                 } label: {
                     Image(systemName: "gearshape.circle.fill")
-                        .resizable()
-                        .frame(width: 40, height: 40)
-                        .foregroundStyle(contrast_color)
-                        .animated_flip(axis: (1,1,0))
+                        .settings_icon(color: settings.color_scheme["contrast"]!)
                 }
-                .padding(.trailing, 20.0)
-                .frame(maxWidth: 80, maxHeight: 20)
-                    
             }
-
         }.padding()
+        .environment(\.layoutDirection, settings.lefty ? .rightToLeft : .leftToRight)
     }
 }
 
@@ -202,10 +266,23 @@ extension View {
     }
 }
 
+extension Image {
+    func settings_icon(color: Color) -> some View {
+        self
+            .resizable()
+            .frame(width: 40, height: 40)
+            .foregroundStyle(color)
+            .rotation3DEffect(.degrees(180), axis: (-1,-1,0))
+            .frame(maxWidth: 80, maxHeight: 20)
+            .padding(.trailing, 20.0)
+            .animated_flip(axis: (1,1,0))
+    }
+}
+
 struct flipAnimation: ViewModifier {
     var axis: (CGFloat, CGFloat, CGFloat)
     @State var animation_value = 0.0
-    func body(content: Content) -> some View{
+    func body(content: Content) -> some View {
         return content
             .scaleEffect(animation_value)
             .rotation3DEffect(.degrees(animation_value*180),axis: axis)
